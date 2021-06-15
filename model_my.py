580a3bea6ae6fd7965e77fc1bfb392f7d9280306
Jaehyeon Myung <mjhbest@kaist.ac.kr>
2021-06-15 21:16:06 +0900

    resUnet added

--- model_my.py
+++ model_my.py
@@ -113,6 +113,125 @@ class UNet(nn.Module):
 
         return F.upsample_bilinear(self.final(enc1), x.size()[2:])
 
+class up_conv(nn.Module):
+    def __init__(self,ch_in,ch_out):
+        super().__init__()
+        self.up = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(ch_in,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        x = self.up(x)
+        return x
+
+class Recurrent_block(nn.Module):
+    def __init__(self,ch_out,t=2):
+        super().__init__()
+        self.t = t
+        self.ch_out = ch_out
+        self.conv = nn.Sequential(
+            nn.Conv2d(ch_out,ch_out,kernel_size=3,stride=1,padding=1,bias=True),
+            nn.BatchNorm2d(ch_out),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self,x):
+        for i in range(self.t):
+
+            if i==0:
+                x1 = self.conv(x)
+            
+            x1 = self.conv(x+x1)
+        return x1
+
+class ResidualBlock(nn.Module):
+    def __init__(self,ch_in,ch_out,t=2):
+        super().__init__()
+        self.recnn = nn.Sequential(
+            Recurrent_block(ch_out,t=t),
+            Recurrent_block(ch_out,t=t)
+        )
+        self.Conv_1x1 = nn.Conv2d(ch_in,ch_out,kernel_size=1,stride=1,padding=0)
+
+    def forward(self,x):
+        x = self.Conv_1x1(x)
+        x1 = self.recnn(x)
+        return x+x1
+
+class ReUNet(nn.Module):
+    def __init__(self,img_ch=3,output_ch=10,t=2):
+        super().__init__()
+        
+        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+        self.Upsample = nn.Upsample(scale_factor=2)
+
+        self.rdec1 = ResidualBlock(ch_in=img_ch,ch_out=64,t=t)
+
+        self.rdec2 = ResidualBlock(ch_in=64,ch_out=128,t=t)
+        
+        self.rdec3 = ResidualBlock(ch_in=128,ch_out=256,t=t)
+        
+        self.rdec4 = ResidualBlock(ch_in=256,ch_out=512,t=t)
+        
+        self.rdec5 = ResidualBlock(ch_in=512,ch_out=1024,t=t)
+        
+
+        self.upconv5 = up_conv(ch_in=1024,ch_out=512)
+        self.renc5 = ResidualBlock(ch_in=1024, ch_out=512,t=t)
+        
+        self.upconv4 = up_conv(ch_in=512,ch_out=256)
+        self.renc4 = ResidualBlock(ch_in=512, ch_out=256,t=t)
+        
+        self.upconv3 = up_conv(ch_in=256,ch_out=128)
+        self.renc3 = ResidualBlock(ch_in=256, ch_out=128,t=t)
+        
+        self.upconv2 = up_conv(ch_in=128,ch_out=64)
+        self.renc2 = ResidualBlock(ch_in=128, ch_out=64,t=t)
+
+        self.Conv_1x1 = nn.Conv2d(64,output_ch,kernel_size=1,stride=1,padding=0)
+
+
+    def forward(self,x):
+        # encoding path
+        x1 = self.rdec1(x)
+
+        x2 = self.Maxpool(x1)
+        x2 = self.rdec2(x2)
+        
+        x3 = self.Maxpool(x2)
+        x3 = self.rdec3(x3)
+
+        x4 = self.Maxpool(x3)
+        x4 = self.rdec4(x4)
+
+        x5 = self.Maxpool(x4)
+        x5 = self.rdec5(x5)
+
+        # decoding + concat path
+        d5 = self.upconv5(x5)
+        d5 = torch.cat((x4,d5),dim=1)
+        d5 = self.renc5(d5)
+        
+        d4 = self.upconv4(d5)
+        d4 = torch.cat((x3,d4),dim=1)
+        d4 = self.renc4(d4)
+
+        d3 = self.upconv3(d4)
+        d3 = torch.cat((x2,d3),dim=1)
+        d3 = self.renc3(d3)
+
+        d2 = self.upconv2(d3)
+        d2 = torch.cat((x1,d2),dim=1)
+        d2 = self.renc2(d2)
+
+        d1 = self.Conv_1x1(d2)
+
+        return d1
+
+
 
 if __name__ == "__main__":
     batch = torch.zeros(64, 3, 256, 256)
